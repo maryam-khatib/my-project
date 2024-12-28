918d90fef37655800962855278c321fe8e6d677b
maryam-khatib <mariam2005lkhatib@gmail.com>
2024-12-28 01:38:26 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Main from './BasicsJS/Main';
+import Main  from './BasicsJS/Main';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
+<>
 <Main />
+</>
 );
 
 // If you want to start measuring performance in your app, pass a function
